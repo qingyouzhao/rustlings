a4c4af8add41c155ec096c2cbe30296763bbae65
Qingyou Zhao <zhaoqingyou315@gmail.com>
2020-06-23 19:50:09 -0700

    Finished? I still feel I barely know this

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,7 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
+// I AM DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -21,6 +21,19 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(String::from("empty string"))
+        }
+        let split:Vec<&str> = s.split(",").collect();
+        if split.len() < 2 {
+            return Err(String::from("no age"))
+        }
+        let name = split[0];
+        let age = split[1];
+        Ok(Person{
+            name: String::from(name),
+            age: age.parse::<usize>().unwrap()
+        })
     }
 }
 
